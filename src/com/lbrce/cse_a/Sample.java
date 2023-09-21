347c9e16ff919833065e26893dabde35f50188be
AdmiN <AdmiN@ITWS60>
2023-09-21 14:02:13 +0530

    Team lead commit

---
+++ src/com/lbrce/cse_a/Sample.java
@@ -0,0 +1,16 @@
+package com.lbrce.cse_a;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void sreya() {
+		// TODO Auto-generated method stub
+		System.out.println("this is team lead - sreya");
+		
+	}
+
+}
